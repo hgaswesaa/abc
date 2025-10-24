15c43e4346923e75668558da701fb9a91b6b7497
hgaswesaa <pataikavidu@gmail.com>
2025-10-24 21:17:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,3 +9,4 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'GcEjQahb#QfhCywlBSiI8Ct3BIRo
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
+//0
